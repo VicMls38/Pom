ffc15cf8014f7e97887783e527870604a8ea06c6
Victor <victor.morales@outlook.fr>
2023-11-10 22:44:30 +0100

    Login

--- src/Models/model.js
+++ src/Models/model.js
@@ -4,9 +4,18 @@ var db = require('../../db');
 
 module.exports={
 
-    Connexion:function(callback, cli_mail, cli_mdp){
+    Inscription:function(user_pseudo, user_email, user_password){
+        var sql="INSERT INTO users(Pseudo, Email, Password ) VALUES ('"+user_pseudo+"','"+user_email+"','"+user_password+"')";
+            db.query(sql, function (err, result) {
+                if (err) throw err;
+                console.log(result.affectedRows + " record(s) created");
+              });
 
-        var sql="SELECT * FROM users WHERE User_ID = 1";
+    },
+    
+    Connexion:function(callback, user_email, user_password){
+        console.log("Before DB : ", user_email, user_password)
+        var sql="SELECT * FROM users WHERE Email = '"+user_email+"' AND Password = '"+user_password+"'";
         db.query(sql, function (err, data, fields){
             if (err){throw err};
                 console.log(data);
--- src/Routes/route.js
+++ src/Routes/route.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const Controller = require('../Controllers/controller');
 
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 //création du routeur Express pour ce module
 const routeur = express.Router();
 routeur.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +14,10 @@ routeur.use(bodyParser.urlencoded({ extended: true }));
 routeur.get('/', Controller.Accueil);
 
 routeur.get('/connexion', Controller.Connexion);
+routeur.post('/login' ,urlencodedParser, Controller.Login);
+
+routeur.get('/inscription', Controller.Inscription)
+routeur.post('/register' ,urlencodedParser, Controller.Register);
   
 
 
